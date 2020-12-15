07dde378f46a71c30eb567e377f9c8d10d969853
fmitchell <fredric@brightplum.com>
2020-12-15 13:46:03 -0600

    homepage header, scattered; carousel

--- js/custom.js
+++ js/custom.js
@@ -41,7 +41,6 @@
 
 
     //Card-slider
-
     $('.card-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -51,9 +50,7 @@
         nextArrow: $('.slider-controls--right')
     });
 
-
     // Plain slider
-
     $('.plain-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -62,6 +59,52 @@
         prevArrow: $('.plain-left'),
         nextArrow: $('.plain-right'),
         dots: true,
+    });
+
+    // text Slider
+
+    $('.text-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+
+    // Carousel
+
+    $('.carousel-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        centerPadding: '60px',
+        centerMode: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '60px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 
