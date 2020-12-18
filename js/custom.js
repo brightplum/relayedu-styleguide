022e1fab7d49ac6d33fcaaa5c9e6aa32f4bff864
fmitchell <fredric@brightplum.com>
2020-12-17 20:34:25 -0600

    news and stories, program tabs, article image header, article stack

--- js/custom.js
+++ js/custom.js
@@ -133,4 +133,35 @@
     });
 
 
+    // Program tabs
+    const select = document.querySelectorAll('.selectBtn');
+    const option = document.querySelectorAll('.option');
+    let index = 1;
+
+    select.forEach(a => {
+        a.addEventListener('click', b => {
+            const next = b.target.nextElementSibling;
+            next.classList.toggle('toggle');
+            next.style.zIndex = index++;
+        });
+    });
+    option.forEach(a => {
+        a.addEventListener('click', b => {
+            b.target.parentElement.classList.remove('toggle');
+
+            const parent = b.target.closest('.select').children[0];
+            parent.setAttribute('data-type', b.target.getAttribute('data-type'));
+            parent.innerText = b.target.innerText;
+        });
+    });
+
+    $('.program-tabs--headers-head').click(function () {
+        let target = $(this).data('target');
+        $('.program-tabs--headers-head').removeClass('active');
+        $(this).addClass('active');
+        $('.program-tabs-tab').removeClass('active');
+        $('#' + target).addClass('active');
+    });
+
+
 })(jQuery)
